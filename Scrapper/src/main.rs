47bbde3523b86b1a38d40f43fb6915ce0a96374b
pikol93 <pikol93@wp.pl>
2023-11-15 12:16:00 +0100

    BE-24 Added a basic product href scrapper

--- Scrapper/src/main.rs
+++ Scrapper/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::blocking;
+use scraper::{Html, Selector};
+use scraper::node::Element;
+
+fn main() {    
+    let response = blocking::get("https://e-okularnicy.pl/10-oprawy")
+        .expect("Should get response from the url");
+    let response_text = response.text()
+        .expect("Result should be text.");
+
+    parse_page(&response_text);
+}
+
+fn parse_page(page_string: &String) {
+    let selector = Selector::parse("a.tile__product").unwrap();
+
+    let document = Html::parse_document(&page_string);
+    document
+        .select(&selector)
+        .map(|selected| selected.value())
+        .for_each(parse_item);
+}
+
+fn parse_item(element: &Element) {
+    let a = element.attr("href");
+    let Some(b) = a else {
+        println!("Could not find href");
+        return;
+    };
+
+    println!("item href: {}", b);
 }
