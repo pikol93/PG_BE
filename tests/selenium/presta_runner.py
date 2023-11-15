4ae76734ce04a275f807376505f9c4d986aac33b
michalszulowski <michal.szulows@gmail.com>
2023-11-15 20:13:08 +0100

    Develop selecting products

--- tests/selenium/presta_runner.py
+++ tests/selenium/presta_runner.py
@@ -1,10 +1,13 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+
 class PrestaRunner():
     def __init__(self, web_driver, url_base):
         self.web_driver = web_driver
         self.url_base = url_base
 
     def add_10_products_to_cart(self):
-        pass
+        self.__select_first_category()
 
     def search_for_product_and_add_to_cart(self):
         pass
@@ -18,3 +21,16 @@ class PrestaRunner():
     def make_order(self):
         pass
 
+    def __select_first_category(self):
+        actions = ActionChains(self.web_driver)
+        cat_elem = self.web_driver.find_element(By.XPATH, '//*[@id="category-3"]/a')
+        actions.move_to_element(cat_elem)
+        category = self.web_driver.find_element(By.XPATH, '//*[@id="category-4"]/a')
+        actions.click(category)
+        actions.perform()
+
+    def __select_products_from_category(self, count):
+        pass
+
+    def __get_nth_element_xpath(self, index):
+        return f'//*[@id="js-product-list"]/div[1]/div[{index}]/article/div/div[1]/a/img'
--- tests/selenium/selenium-tests.py
+++ tests/selenium/selenium-tests.py
@@ -1,5 +1,6 @@
 from chromedriver_provider import ChromeDriverProvider
 from presta_runner import PrestaRunner
+import time
 
 driver = ChromeDriverProvider().build_chromedriver()
 url_base = 'https://localhost:8001/'
@@ -8,5 +9,7 @@ presta_runner = PrestaRunner(driver, url_base)
 driver.get(url_base)
 
 presta_runner.add_10_products_to_cart()
+
+time.sleep(5)
 
 driver.close()
