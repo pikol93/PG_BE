1e26e8e903db8c6b242db3b9663dc6bb034fc526
pikol93 <pikol93@wp.pl>
2023-11-20 15:01:14 +0100

    BE-78 Removed unused Prestashop directory

--- Prestashop/.docker/ssl.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir /tmp/ssl
-touch /tmp/ssl/apache-selfsigned.key
-touch /tmp/ssl/apache-selfsigned.crt
-openssl req -newkey rsa:2048 -new -nodes -x509 -days 365 -keyout /tmp/ssl/apache-selfsigned.key -out /tmp/ssl/apache-selfsigned.crt -subj "/C=PL/ST=Pomerania/L=Gdansk/O=PG/OU=./CN=."
-cp /tmp/ssl/apache-selfsigned.key /etc/ssl/private/apache-selfsigned.key
-cp /tmp/ssl/apache-selfsigned.crt /etc/ssl/certs/apache-selfsigned.crt
