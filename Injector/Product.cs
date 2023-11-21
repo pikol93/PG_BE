f6734cfd0f9243f90ae1bb009b806fb1fa59c4c7
pikol93 <pikol93@wp.pl>
2023-11-21 20:52:59 +0100

    BE-54 Added OptionalProductPropertyAttribute and fetching optional properties

--- Injector/Product.cs
+++ Injector/Product.cs
@@ -1,9 +1,12 @@
 using Newtonsoft.Json;
+using NLog;
 
 namespace Injector;
 
 public class Product
 {
+    private static ILogger Logger { get; } = LogManager.GetCurrentClassLogger();
+
     [JsonProperty("id")]
     public int Id { get; set; }
 
@@ -14,7 +17,54 @@ public class Product
     public string Subcategory { get; set; }
 
     [JsonProperty("Kolor")]
+    [OptionalProductProperty]
     public string? Color { get; set; }
+
+    public Dictionary<string, string> GetOptionalProperties()
+    {
+        var propertyInfos = typeof(Product).GetProperties();
+        var result = new Dictionary<string, string>();
+
+        foreach (var propertyInfo in propertyInfos)
+        {
+            string jsonPropertyName = null;
+            var attributes = propertyInfo.GetCustomAttributes(true);
+            var isOptional = false;
+            foreach (var attribute in attributes)
+            {
+                switch (attribute)
+                {
+                    case OptionalProductPropertyAttribute:
+                        isOptional = true;
+                        break;
+                    case JsonPropertyAttribute property:
+                        jsonPropertyName = property.PropertyName;
+                        break;
+                }
+            }
+
+            if (!isOptional)
+            {
+                continue;
+            }
+
+            if (jsonPropertyName == null)
+            {
+                throw new Exception("JSON property should have a name");
+            }
+
+            var propertyValue = propertyInfo.GetValue(this) as string;
+            if (propertyValue == null)
+            {
+                continue;
+            }
+
+            Logger.Trace("Adding property value: {}: {}", jsonPropertyName, propertyValue);
+            result[jsonPropertyName] = propertyValue;
+        }
+
+        return result;
+    }
 
     public override string ToString()
     {
