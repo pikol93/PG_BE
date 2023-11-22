6bbf0816ed500fcb95ef222b2419848300ed7a64
pikol93 <pikol93@wp.pl>
2023-11-22 10:56:08 +0100

    BE-54 Updated injector settings to include PrestaShop data

--- Injector/InjectorSettings.cs
+++ Injector/InjectorSettings.cs
@@ -4,4 +4,7 @@ public interface InjectorSettings
 {
     string CategoriesFilePath { get; }
     string ScrappedFilePath { get; }
+    string PrestaShopBaseUrl { get; }
+    string PrestaShopAccount { get; }
+    string PrestaShopSecretKey { get; }
 }
