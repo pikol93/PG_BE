c949562566d16a0505605798957ee6f408ab6809
pikol93 <pikol93@wp.pl>
2023-11-23 18:20:39 +0100

    BE-82 Updated InjectorSettings

--- Injector/InjectorSettings.cs
+++ Injector/InjectorSettings.cs
@@ -4,6 +4,7 @@ public interface InjectorSettings
 {
     string CategoriesFilePath { get; }
     string ScrappedFilePath { get; }
+    string ImagesDirectoryFilePath { get; }
     string PrestaShopBaseUrl { get; }
     string PrestaShopAccount { get; }
     string PrestaShopSecretKey { get; }
