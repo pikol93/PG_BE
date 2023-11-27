cbeea4a2807ce0105eff0d55c1718a5a0781197f
Sladamos <sladamo@wp.pl>
2023-11-27 21:08:16 +0100

    BE-86 splitting too large images archive

--- Scripts/create_dump.sh
+++ Scripts/create_dump.sh
@@ -5,6 +5,8 @@ PRESTASHOP_CONTAINER_ID=$2
 PASS=$3
 DATABASE=$4
 docker exec $PRESTASHOP_CONTAINER_ID bash -c "tar -czvf images.tar.gz /var/www/html/img/"
-docker cp $PRESTASHOP_CONTAINER_ID:/var/www/html/images.tar.gz ./images.tar.gz
+docker cp $PRESTASHOP_CONTAINER_ID:/var/www/html/images.tar.gz images.tar.gz
 docker exec $PRESTASHOP_CONTAINER_ID bash -c "rm -rf images.tar.gz"
+split -b 20M images.tar.gz "images.tar.gz.part"
+rm images.tar.gz
 docker exec $DATABASE_CONTAINER_ID bash -c "usr/bin/mysqldump -u root --password=${PASS} --databases ${DATABASE}" > ./dump.sql
