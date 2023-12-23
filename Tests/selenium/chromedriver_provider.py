d8ff3dc1eeabebcbff469871869b9a40b3f0f989
Sladamos <sladamos@wp.pl>
2023-12-23 21:40:33 +0100

    techno

--- Tests/selenium/chromedriver_provider.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-class ChromeDriverProvider():
-    def build_chromedriver(self):
-        driver_path = '/usr/bin/chromedriver'
-        options = webdriver.ChromeOptions()
-        options.add_argument('--ignore-ssl-errors=yes')
-        options.add_argument('--ignore-certificate-errors')
-        return webdriver.Chrome(driver_path, options=options)
